d64e46a207fb27c680fb7ebea682e9395d5ad65f
hindoz <hindamir.99@gmail.com>
2023-09-19 11:12:27 -0700

    upload main and printf file

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 int _printf(const char *format, ...);
-
+int main_printf(const char *format, va_list ptr, buf_t *output);
 
 /**
  * _printf - print its arguments based on the specifier.
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 {
 	buf_t *output;
 	va_list ptr;
-	int return_value;
+	int retrn_value;
 
 	if (format == NULL)
 		return (-1);
@@ -23,9 +23,9 @@ int _printf(const char *format, ...)
 
 	va_start(ptr, format);
 
-	return_value = main_printf(format, ptr, output);
+	retrn_value = main_printf(format, ptr, output);
 
-	return (return_value);
+	return (retrn_value);
 }
 
 /**
@@ -38,7 +38,7 @@ int _printf(const char *format, ...)
  */
 int main_printf(const char *format, va_list ptr, buf_t *output)
 {
-	int i, return_value = 0;
+	int i, retrn_value = 0;
 	char tmp;
 	unsigned char length;
 	unsigned int (*f)(va_list, buf_t *,
@@ -52,8 +52,8 @@ int main_printf(const char *format, va_list ptr, buf_t *output)
 			tmp = 0;
 			flag = test_flags(format + i + 1, &tmp);
 			width = test_width(ptr, format + i + tmp + 1, &tmp);
-			prec = test_precision(ptr, format + i + tmp + 1,&tmp);
-		
+			prec = test_precision(ptr, format + i + tmp + 1, &tmp);
+
 			length = handle_length(format + i + tmp + 1, &tmp);
 
 			f = test_specifiers(format + i + tmp + 1);
@@ -65,13 +65,13 @@ int main_printf(const char *format, va_list ptr, buf_t *output)
 			}
 			else if (*(format + i + tmp + 1) == '\0')
 			{
-				return_value = -1;
+				retrn_value = -1;
 				break;
 			}
 		}
-		return_value += _memcpy(output, (format + i), 1);
+		retrn_value += _memcpy(output, (format + i), 1);
 		i += (length != 0) ? 1 : 0;
 	}
 	cleanup(args, output);
-	return (return_value);
+	return (retrn_value);
 }
